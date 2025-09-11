62a70dbd71a5a3286468c3917840c67facdbe402
Idiom <idiom444@gmail.com>
2025-09-11 16:17:13 -0700

    Rotation working. Can't figure out how to get debuff duration.

--- Priest/Lua/Shadow.lua
+++ Priest/Lua/Shadow.lua
@@ -1,23 +1,24 @@
 local _G, setmetatable           = _G, setmetatable
-local TMW                        = _G.TMW
-local Action                     = _G.Action
-local Create                     = Action.Create
-
--- Defs i might need later
-
---local CNDT              = TMW.CNDT
---local Env               = CNDT.Env
---local Player            = Action.Player
---local BurstIsON         = Action.BurstIsON
---local MultiUnits        = Action.MultiUnits
---local ActiveUnitPlates  = MultiUnits:GetActiveUnitPlates()
---local GetToggle         = Action.GetToggle
---local player            = "player"
---local target            = "target"
---local targettarget      = "targettarget"
---local mouseover         = "mouseover"
---local focustarget       = "focustarget"
---local focus             = "focus"
+local TMW = _G.TMW
+local Action = _G.Action
+local GetToggle = Action.GetToggle
+local GetGCD = Action.GetGCD
+local GetCurrentGCD = Action.GetCurrentGCD
+local BurstIsON = Action.BurstIsON
+local EnemyTeam = Action.EnemyTeam
+local Player = Action.Player
+local MultiUnits = Action.MultiUnits
+local Unit = Action.Unit
+local IsMounted = _G.IsMounted
+local UnitCanAttack = _G.UnitCanAttack
+local UnitDetailedThreatSituation = _G.UnitDetailedThreatSituation
+local IsResting = _G.IsResting
+local InCombatLockdown = _G.InCombatLockdown
+local IsUsableSpell = _G.IsUsableSpell
+local CombatLogGetCurrentEventInfo = _G.CombatLogGetCurrentEventInfo
+local UnitGUID = _G.UnitGUID
+local player = "player"
+local target = "target"
 
 Action[Action.PlayerClass] = {
     -- Base Abilities
@@ -93,19 +94,93 @@ local A = setmetatable(Action[Action.PlayerClass], { __index = Action })
 
 A[1] = nil
 A[2] = nil
-A[3] = function(icon)
-    
-    local function DamageRotation(unit)         
-        return A.MindFlay
+local function TargetRotation(icon, unit)
+    local targetUnit = Action.Unit(unit)
+    if not A.IsUnitEnemy(unit) then
+        return false
     end
-    
-    if A.IsUnitEnemy("target") then
-        local unit = "target"
-        local action = DamageRotation(unit)        
-        if action then            
-            return action:Show(icon)        
+    if not targetUnit:IsExists() then
+        return false
+    end
+    if targetUnit:IsDead() then
+        return false
+    end
+    if not A.MindBlast:IsSpellInRange(unit) then
+        return false
+    end
+    local shadowOrbs = UnitPower("player", 6) or 0
+    local targetHP = targetUnit:HealthPercent()
+    local hasTwistOfFate = Action.Unit("player"):HasBuffs(A.TwistOfFate.ID) > 0
+    local hasPowerInfusion = Action.Unit("player"):HasBuffs(A.PowerInfusion.ID) > 0
+    local hasDivineInsight = Action.Unit("player"):HasBuffs(A.DivineInsight.ID) > 0
+    local nearbyEnemies = Action.MultiUnits:GetByRange(10, 5)
+    if nearbyEnemies >= 6 then
+        if A.MindSear:IsReady(unit) and A.MindSear:IsSpellInRange(unit) then
+            return A.MindSear:Show(icon)
         end
-    end    
+        return false
+    end
+    if A.PowerInfusion:IsReady(unit) and A.PowerInfusion:IsSpellInRange(unit) then
+        return A.PowerInfusion:Show(icon)
+    end
+    if targetHP <= 20 and A.SWD:IsReady(unit) and A.SWD:IsSpellInRange(unit) then
+        return A.SWD:Show(icon)
+    end
+    if shadowOrbs >= 3 and A.DevouringPlague:IsReady(unit) and A.DevouringPlague:IsSpellInRange(unit) then
+        return A.DevouringPlague:Show(icon)
+    end
+    if A.MindBlast:IsReady(unit) and A.MindBlast:IsSpellInRange(unit) then
+        return A.MindBlast:Show(icon)
+    end
+    if A.ShadowFiend:IsReady(unit) and A.ShadowFiend:IsSpellInRange(unit) then
+        return A.ShadowFiend:Show(icon)
+    end
+    if A.Mindbender:IsReady(unit) and A.Mindbender:IsSpellInRange(unit) then
+        return A.Mindbender:Show(icon)
+    end
+    if A.Halo:IsReady(unit) and A.Halo:IsSpellInRange(unit) then
+        return A.Halo:Show(icon)
+    end
+    if A.DivineStar:IsReady(unit) and A.DivineStar:IsSpellInRange(unit) then
+        return A.DivineStar:Show(icon)
+    end
+    if A.Cascade:IsReady(unit) and A.Cascade:IsSpellInRange(unit) then
+        return A.Cascade:Show(icon)
+    end
+    local swpDuration = targetUnit:HasDeBuffs(A.SWP.ID)
+    if A.SWP:IsReady(unit) and A.SWP:IsSpellInRange(unit) and swpDuration == 0 then
+        return A.SWP:Show(icon)
+    end
+    local vtDuration = targetUnit:HasDeBuffs(A.VampTouch.ID)
+    if A.VampTouch:IsReady(unit) and A.VampTouch:IsSpellInRange(unit) and vtDuration == 0 then
+        return A.VampTouch:Show(icon)
+    end
+    if A.Solace:IsReady(unit) and A.Solace:IsSpellInRange(unit) then
+        return A.Solace:Show(icon)
+    end
+    local dpDuration = targetUnit:HasDeBuffs(A.DevouringPlague.ID)
+    if A.MindFlay:IsReady(unit) and A.MindFlay:IsSpellInRange(unit) and dpDuration > 0 then
+        return A.MindFlay:Show(icon)
+    end
+    if nearbyEnemies >= 2 and A.MindSear:IsReady(unit) and A.MindSear:IsSpellInRange(unit) then
+        return A.MindSear:Show(icon)
+    end
+    if A.MindFlay:IsReady(unit) and A.MindFlay:IsSpellInRange(unit) then
+        return A.MindFlay:Show(icon)
+    end
+    return false
+end
+
+A[3] = function(icon)
+    local isInShadowForm = Action.Unit("player"):HasBuffs(A.ShadowForm.ID) > 0
+    local hasInnerFire = Action.Unit("player"):HasBuffs(A.InnerFire.ID) > 0
+    if not isInShadowForm and A.ShadowForm:IsReady() then
+        return A.ShadowForm:Show(icon)
+    end
+    if not hasInnerFire and A.InnerFire:IsReady() then
+        return A.InnerFire:Show(icon)
+    end
+    return TargetRotation(icon, "target")
 end
 A[4] = nil
 A[5] = nil
