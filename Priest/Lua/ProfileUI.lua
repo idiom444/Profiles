8465089dd500d1c39be09e87de217df8906807c5
Idiom <idiom444@gmail.com>
2025-09-13 19:43:22 -0700

    Version 1.0 pretty solid

--- Priest/Lua/ProfileUI.lua
+++ Priest/Lua/ProfileUI.lua
@@ -1,6 +1,6 @@
-local _G                         = _G
-local Action                     = _G.Action
-local A                          = Action
+local _G, setmetatable = _G, setmetatable
+local TMW = _G.TMW 
+local A = _G.Action
 
 -- Defs I might need later
 
@@ -11,18 +11,73 @@ local A                          = Action
 
 A.Data.ProfileEnabled[A.CurrentProfile] = true 
 
-A.Data.ProfileUI = {
-    DateTime = "recently updated",
-    [2] = {
-        {
+A.Data.ProfileUI = {	
+    DateTime = "v1.0 (13.09.2025)",
+    [2] = {         						
             {
-                E = "Header",
-                L = {
-                    enUS = "Priest yeetlol",                        
+                {	
+                    E = "Header",
+                    L = { 
+                        enUS = "Shadow Priest Settings", 
+                        ruRU = "Настройки Теневого Жреца", 
+                    }, 
+                    S = 14,
                 },
-                S = 14,
+            },					
+            {							
+                {
+                    E = "Slider", 													
+                    H = 20,
+                    MIN = 1, 
+                    MAX = 100,							
+                    DB = "DesperatePrayerHP",
+                    DBV = 40,
+                    L = { 
+                        enUS = "Desperate Prayer HP %", 
+                        ruRU = "Отчаянная молитва ХП %", 
+                    }, 
+                    TT = { 
+                        enUS = "Cast Desperate Prayer when HP drops below this percentage", 
+                        ruRU = "Использовать Отчаянную молитву когда ХП падает ниже этого процента", 
+                    }, 
+                    M = {},
+                    Precision = 1,
+                },
+                {
+                    E = "Slider", 													
+                    H = 20,
+                    MIN = 1, 
+                    MAX = 100,							
+                    DB = "DispersionHP",
+                    DBV = 20,
+                    L = { 
+                        enUS = "Dispersion HP %", 
+                        ruRU = "Рассеивание ХП %", 
+                    }, 
+                    TT = { 
+                        enUS = "Cast Dispersion when HP drops below this percentage", 
+                        ruRU = "Использовать Рассеивание когда ХП падает ниже этого процента", 
+                    }, 
+                    M = {},
+                    Precision = 1,
+                },
             },
-        },
-    }
+            {
+                {
+                    E = "Checkbox", 
+                    DB = "UseDispersion",
+                    DBV = true,
+                    L = { 
+                        enUS = "Enable Dispersion", 
+                        ruRU = "Включить Рассеивание", 
+                    }, 
+                    TT = { 
+                        enUS = "Enable automatic Dispersion casting when HP is low", 
+                        ruRU = "Включить автоматическое использование Рассеивания при низком ХП", 
+                    }, 
+                    M = {},
+                },
+            },        
+    },
 }
 
--- Priest/Lua/Shadow.lua
+++ Priest/Lua/Shadow.lua
@@ -2,6 +2,7 @@ local _G, setmetatable           = _G, setmetatable
 local TMW = _G.TMW
 local Action = _G.Action
 local Create = Action.Create
+local Listener = Action.Listener
 local GetToggle = Action.GetToggle
 local GetGCD = Action.GetGCD
 local GetCurrentGCD = Action.GetCurrentGCD
@@ -76,8 +77,9 @@ Action[Action.PlayerClass] = {
     DivineInsight = Create({ Type = "Spell", ID = 109175 }),
     Cascade = Create({ Type = "Spell", ID = 121135 }),
     DivineStar = Create({ Type = "Spell", ID = 110744 }),
-    Halo = Create({ Type = "Spell", ID = 120517 }),
+    Halo = Create({ Type = "Spell", ID = 120644 }),
     QuakingPalm = Create({ Type = "Spell", ID = 107719 }),
+    Insanity = Create({ Type = "Spell", ID = 129197 }),
     Lightweave = Create({ Type = "Spell", ID = 55637 }),
     Darkglow = Create({ Type = "Spell", ID = 55775 }),
     SynapseSprings = Create({ Type = "Spell", ID = 96230 }),
@@ -100,65 +102,11 @@ Action[Action.PlayerClass] = {
 
 local A = setmetatable(Action[Action.PlayerClass], { __index = Action })
 
+local swaps = 0
+local lastEnemyCount = 0
+
 A[1] = nil
 A[2] = nil
-local lastDamageCalculation = 0
-local lastBuffCount = 0
-local damageIncreaseThreshold = 0.15
-
-local function getCurrentSpellPower()
-    local intellect = UnitStat("player", 4) or 0
-    local spellPower = intellect * 1.0
-    local baseSpellPower = GetSpellBonusDamage(6) or 0
-    spellPower = spellPower + baseSpellPower
-    return spellPower
-end
-
-local function getCurrentCritChance()
-    local critRating = GetCombatRating(9) or 0
-    local critChance = critRating / 600
-    return critChance
-end
-
-local function calculateCurrentDamage()
-    local spellPower = getCurrentSpellPower()
-    local critChance = getCurrentCritChance()
-    local baseDamage = spellPower * (1 + critChance)
-    return baseDamage
-end
-
-local function hasNewBuffs()
-    local currentBuffCount = 0
-    local i = 1
-    while UnitBuff("player", i) do
-        currentBuffCount = currentBuffCount + 1
-        i = i + 1
-    end
-    if currentBuffCount ~= lastBuffCount then
-        lastBuffCount = currentBuffCount
-        return true
-    end
-    return false
-end
-
-local function shouldSnapshotDoT(unit, dotID, currentDotRemain)
-    if currentDotRemain > 3 then
-        return false
-    end
-    if hasNewBuffs() then
-        local currentDamage = calculateCurrentDamage()
-        local damageIncrease = (currentDamage - lastDamageCalculation) / lastDamageCalculation
-        lastDamageCalculation = currentDamage
-        if damageIncrease >= damageIncreaseThreshold then
-            return true
-        end
-    end
-    return false
-end
-
-if lastDamageCalculation == 0 then
-    lastDamageCalculation = calculateCurrentDamage()
-end
 
 local function SingleTargetRotation(icon, unit)
     if not A.IsUnitEnemy(unit) then
@@ -173,111 +121,86 @@ local function SingleTargetRotation(icon, unit)
     if not A.MindBlast:IsSpellInRange(unit) then
         return false
     end
+    swaps = 0
     local shadowOrbs = UnitPower("player", Enum.PowerType.ShadowOrbs) or 0
     local targetHP = Action.Unit(unit):HealthPercent()
-    local hasTwistOfFate = Action.Unit("player"):HasBuffs(A.TwistOfFate.ID) > 0
-    local hasPowerInfusion = Action.Unit("player"):HasBuffs(A.PowerInfusion.ID) > 0
-    local hasDivineInsight = Action.Unit("player"):HasBuffs(A.DivineInsight.ID) > 0
     local swpRemain = Action.Unit(unit):HasDeBuffs(A.SWP.ID, true)
     local vtRemain = Action.Unit(unit):HasDeBuffs(A.VampTouch.ID, true)
     local dpRemain = Action.Unit(unit):HasDeBuffs(A.DevouringPlague.ID, true)
+    local insanityRemain = Action.Unit(unit):HasDeBuffs(A.Insanity.ID, true)
     local isCastingVT = Action.Unit("player"):IsCasting() and Action.Unit("player"):IsCasting(A.VampTouch.ID)
-    local isChannelingMF = UnitChannelInfo("player") == GetSpellInfo(A.MindFlay.ID)
+    local channelingSpell = Action.Player:IsChanneling()
+    local isChannelingMF = channelingSpell == GetSpellInfo(A.MindFlay.ID)
     
-    if A.PowerInfusion:IsReady(unit) and A.PowerInfusion:IsSpellInRange(unit) then
+    if dpRemain == 0 and insanityRemain == 0 and A.PowerInfusion:IsReady(unit) and A.PowerInfusion:IsSpellInRange(unit) then
         if isChannelingMF then
-            Action.StopCasting()
+            return A:Show(icon, Action.Const.STOPCAST)
         end
         return A.PowerInfusion:Show(icon)
     end
-    if targetHP <= 20 and A.SWD:IsReady(unit) and A.SWD:IsSpellInRange(unit) then
+    if dpRemain == 0 and insanityRemain == 0 and A.ShadowFiend:IsReady(unit) and A.ShadowFiend:IsSpellInRange(unit) then
         if isChannelingMF then
-            Action.StopCasting()
-        end
-        return A.SWD:Show(icon)
-    end
-    if A.SWP:IsReady(unit) and A.SWP:IsSpellInRange(unit) then
-        if not swpRemain or swpRemain <= 3 then
-            if isChannelingMF then
-                Action.StopCasting()
-            end
-            return A.SWP:Show(icon)
-        else
-            local shouldSnapshot = shouldSnapshotDoT(unit, A.SWP.ID, swpRemain)
-            if shouldSnapshot then
-                if isChannelingMF then
-                    Action.StopCasting()
-                end
-                return A.SWP:Show(icon)
-            end
-        end
-    end
-    if A.VampTouch:IsReady(unit) and A.VampTouch:IsSpellInRange(unit) and not isCastingVT then
-        if not vtRemain or vtRemain <= 3 then
-            if isChannelingMF then
-                Action.StopCasting()
-            end
-            return A.VampTouch:Show(icon)
-        else
-            local shouldSnapshot = shouldSnapshotDoT(unit, A.VampTouch.ID, vtRemain)
-            if shouldSnapshot then
-                if isChannelingMF then
-                    Action.StopCasting()
-                end
-                return A.VampTouch:Show(icon)
-            end
-        end
-    end
-    if shadowOrbs >= 3 and A.DevouringPlague:IsReady(unit) and A.DevouringPlague:IsSpellInRange(unit) then
-        if isChannelingMF then
-            Action.StopCasting()
-        end
-        return A.DevouringPlague:Show(icon)
-    end
-    if A.MindBlast:IsReady(unit) and A.MindBlast:IsSpellInRange(unit) then
-        if isChannelingMF then
-            Action.StopCasting()
-        end
-        return A.MindBlast:Show(icon)
-    end
-    if dpRemain and dpRemain > 0 and A.MindFlay:IsReady(unit) and A.MindFlay:IsSpellInRange(unit) then
-        return A.MindFlay:Show(icon)
-    end
-    if A.ShadowFiend:IsReady(unit) and A.ShadowFiend:IsSpellInRange(unit) then
-        if isChannelingMF then
-            Action.StopCasting()
+            return A:Show(icon, Action.Const.STOPCAST)
         end
         return A.ShadowFiend:Show(icon)
     end
-    if A.Mindbender:IsReady(unit) and A.Mindbender:IsSpellInRange(unit) then
+    if dpRemain == 0 and insanityRemain == 0 and A.Mindbender:IsReady(unit) and A.Mindbender:IsSpellInRange(unit) then
         if isChannelingMF then
-            Action.StopCasting()
+            return A:Show(icon, Action.Const.STOPCAST)
         end
         return A.Mindbender:Show(icon)
     end
-    if A.Halo:IsReady(unit) and A.Halo:IsSpellInRange(unit) then
+    if dpRemain == 0 and shadowOrbs < 3 and targetHP <= 20 and insanityRemain == 0 and A.SWD:IsReady(unit) and A.SWD:IsSpellInRange(unit) then
         if isChannelingMF then
-            Action.StopCasting()
+            return A:Show(icon, Action.Const.STOPCAST)
+        end
+        return A.SWD:Show(icon)
+    end
+    if dpRemain == 0 and insanityRemain == 0 and A.SWP:IsSpellInRange(unit) then
+        if not swpRemain or swpRemain <= 3 then
+            if isChannelingMF then  
+                return A:Show(icon, Action.Const.STOPCAST)
+            end
+            return A.SWP:Show(icon)
+        end
+    end
+    if dpRemain == 0 and insanityRemain == 0 and A.VampTouch:IsReady(unit) and A.VampTouch:IsSpellInRange(unit) and not isCastingVT and (swpRemain and swpRemain >= 3) then
+        if not vtRemain or vtRemain <= 3 then
+            if isChannelingMF then
+                return A:Show(icon, Action.Const.STOPCAST)
+            end
+            return A.VampTouch:Show(icon)
+        end
+    end
+    if dpRemain == 0 and shadowOrbs >= 3 and insanityRemain == 0 and A.DevouringPlague:IsReady(unit) and A.DevouringPlague:IsSpellInRange(unit) and (vtRemain and vtRemain >= 3) and (swpRemain and swpRemain >= 3) then
+        if isChannelingMF then
+            return A:Show(icon, Action.Const.STOPCAST)
+        end
+        return A.DevouringPlague:Show(icon)
+    end
+    if dpRemain == 0 and shadowOrbs < 3 and insanityRemain == 0 and A.MindBlast:IsReady(unit) and A.MindBlast:IsSpellInRange(unit) and (vtRemain and vtRemain >= 3) and (swpRemain and swpRemain >= 3) then
+        if isChannelingMF then
+            return A:Show(icon, Action.Const.STOPCAST)
+        end
+        return A.MindBlast:Show(icon)
+    end
+    if dpRemain == 0 and shadowOrbs < 3 and insanityRemain == 0 and A.Halo:IsTalentLearned() and A.Halo:GetCooldown() == 0 and A.MindFlay:IsSpellInRange(unit) and (vtRemain and vtRemain >= 3) and (swpRemain and swpRemain >= 3) then
+        if isChannelingMF then
+            return A:Show(icon, Action.Const.STOPCAST)
         end
         return A.Halo:Show(icon)
     end
-    if A.DivineStar:IsReady(unit) and A.DivineStar:IsSpellInRange(unit) then
+    if dpRemain == 0 and shadowOrbs < 3 and insanityRemain == 0 and A.DivineStar:IsTalentLearned() and A.DivineStar:GetCooldown() == 0 and A.MindFlay:IsSpellInRange(unit) and (vtRemain and vtRemain >= 3) and (swpRemain and swpRemain >= 3) then
         if isChannelingMF then
-            Action.StopCasting()
+            return A:Show(icon, Action.Const.STOPCAST)
         end
         return A.DivineStar:Show(icon)
     end
-    if A.Cascade:IsReady(unit) and A.Cascade:IsSpellInRange(unit) then
+    if dpRemain == 0 and shadowOrbs < 3 and insanityRemain == 0 and A.Cascade:IsTalentLearned() and A.Cascade:GetCooldown() == 0 and A.MindFlay:IsSpellInRange(unit) and (vtRemain and vtRemain >= 3) and (swpRemain and swpRemain >= 3) then
         if isChannelingMF then
-            Action.StopCasting()
+            return A:Show(icon, Action.Const.STOPCAST)
         end
         return A.Cascade:Show(icon)
-    end
-    if A.Solace:IsReady(unit) and A.Solace:IsSpellInRange(unit) then
-        if isChannelingMF then
-            Action.StopCasting()
-        end
-        return A.Solace:Show(icon)
     end
     if A.MindFlay:IsReady(unit) and A.MindFlay:IsSpellInRange(unit) then
         return A.MindFlay:Show(icon)
@@ -298,37 +221,112 @@ local function AoERotation(icon, unit)
     if not A.MindSear:IsSpellInRange(unit) then
         return false
     end
-    local nearbyEnemies = Action.MultiUnits:GetByRange(10, 5)
+
+    local nearbyEnemies = Action.MultiUnits:GetByRangeInCombat(40)
+    if nearbyEnemies ~= lastEnemyCount then
+        swaps = 0
+        lastEnemyCount = nearbyEnemies
+    end
     if nearbyEnemies >= 6 then
+        swaps = 0
         if A.MindSear:IsReady(unit) and A.MindSear:IsSpellInRange(unit) then
             return A.MindSear:Show(icon)
         end
         return false
     end
-    if nearbyEnemies >= 2 and A.MindSear:IsReady(unit) and A.MindSear:IsSpellInRange(unit) then
-        return A.MindSear:Show(icon)
+    if nearbyEnemies >= 2 then
+        local currentSWP = Action.Unit(unit):HasDeBuffs(A.SWP.ID, true)
+        local currentVT = Action.Unit(unit):HasDeBuffs(A.VampTouch.ID, true)
+        local isCastingVT = Action.Unit("player"):IsCasting() and Action.Unit("player"):IsCasting(A.VampTouch.ID)
+        local enemiesMissingSWP = Action.MultiUnits:GetByRangeMissedDoTs(10, nil, A.SWP.ID, 6)
+        local enemiesMissingVT = Action.MultiUnits:GetByRangeMissedDoTs(10, nil, A.VampTouch.ID, 6)
+        local needsSWP = not currentSWP or currentSWP <= 3
+        local needsVT = not currentVT or currentVT <= 3
+        if needsSWP and enemiesMissingSWP > 0 and A.SWP:IsReady(unit) and A.SWP:IsSpellInRange(unit) then
+            return A.SWP:Show(icon)
+        end
+        if needsVT and currentSWP and enemiesMissingVT > 0 and A.VampTouch:IsReady(unit) and A.VampTouch:IsSpellInRange(unit) and not isCastingVT then
+            return A.VampTouch:Show(icon)            
+        end
+        
+        local currentTargetComplete = (not needsSWP) and (not needsVT)
+        local totalTargetsNeedingDoTs = math.max(enemiesMissingSWP, enemiesMissingVT)
+        if currentTargetComplete and totalTargetsNeedingDoTs > 0 and swaps < totalTargetsNeedingDoTs then
+            swaps = swaps + 1
+            return A:Show(icon, Action.Const.AUTOTARGET)
+        end
+        if nearbyEnemies == 5 then
+            if A.MindSear:IsReady(unit) and A.MindSear:IsSpellInRange(unit) then
+                return A.MindSear:Show(icon)
+            end
+        else
+            return SingleTargetRotation(icon, unit)
+        end
+        if swaps >= nearbyEnemies then
+            swaps = 0
+        end
     end
     return false
 end
 
-local function BuffCheck()
+local function BuffCheck(icon)
     local isInShadowForm = Action.Unit("player"):HasBuffs(A.ShadowForm.ID) > 0
     local hasInnerFire = Action.Unit("player"):HasBuffs(A.InnerFire.ID) > 0
+    local hasPWF = Action.Unit("player"):HasBuffs(A.PWF.ID) > 0
     if not isInShadowForm and A.ShadowForm:IsReady() then
         return A.ShadowForm:Show(icon)
     end
     if not hasInnerFire and A.InnerFire:IsReady() then
         return A.InnerFire:Show(icon)
     end
+    if not hasPWF and A.PWF:IsReady() then
+        return A.PWF:Show(icon)
+    end
+    return false
+end
+
+local function MovementCheck(icon)
+    if Action.Player:IsMoving() and A.AngelicFeather:IsReady() then
+        return A.AngelicFeather:Show(icon)
+    end
+    return false
+end
+
+local function DefensiveCheck(icon)
+    local playerHP = Action.Unit("player"):HealthPercent()
+    local desperatePrayerHP = A.GetToggle(2, "DesperatePrayerHP")
+    local dispersionHP = A.GetToggle(2, "DispersionHP")
+    local useDispersion = A.GetToggle(2, "UseDispersion")
+    
+    -- Emergency healing (highest priority)
+    if playerHP <= desperatePrayerHP and A.DesperatePrayer:IsReady() then
+        return A.DesperatePrayer:Show(icon)
+    end
+    
+    -- Defensive cooldowns (highest priority)
+    if useDispersion and playerHP <= dispersionHP and A.Dispersion:IsReady() then
+        return A.Dispersion:Show(icon)
+    end
+    
     return false
 end
 
 A[3] = function(icon)
-    local buffs = BuffCheck()
+    local buffs = BuffCheck(icon)
     if buffs then
         return buffs
     end
-    local nearbyEnemies = Action.MultiUnits:GetByRange(10, 5)
+    
+    local defensive = DefensiveCheck(icon)
+    if defensive then
+        return defensive
+    end
+    
+    local movement = MovementCheck(icon)
+    if movement then
+        return movement
+    end
+    local nearbyEnemies = Action.MultiUnits:GetByRange(40)
     local rotationSpell = nearbyEnemies >= 2 and AoERotation(icon,"target") or SingleTargetRotation(icon, "target")
     if rotationSpell then
         return rotationSpell
